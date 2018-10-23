a1a89495d8eeabad66962965b3bb5f4afbf6b489
BruceWhiteBai <1258444549@qq.com>
2018-10-23 19:33:39 +0800

    添加cloud配置项

---
+++ web/src/main/java/com/bruce/web/CloudController.java
@@ -0,0 +1,24 @@
+package com.bruce.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cloud")
+public class CloudController {
+
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping("/")
+    public String init(){
+        String services  = client.getServices().toString();
+        return services;
+    }
+
+
+
+}
