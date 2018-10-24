569d22a85034a02b361b1b3658f10324eca0ca05
BruceWhiteBai <1258444549@qq.com>
2018-10-24 19:40:36 +0800

    cloud模块梳理搭建

---
+++ cloud/eureka-server-00/src/main/java/com/bruce/eurekaserver00/EurekaServer00Application.java
@@ -0,0 +1,12 @@
+package com.bruce.eurekaserver00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer00Application.class, args);
+    }
+}
---
+++ cloud/eureka-server-01/src/main/java/com/bruce/eurekaserver01/EurekaServer01Application.java
@@ -0,0 +1,12 @@
+package com.bruce.eurekaserver01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer01Application.class, args);
+    }
+}
