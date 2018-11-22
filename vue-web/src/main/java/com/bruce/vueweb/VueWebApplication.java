502337fad6335452a3520f7d78271efa5a34698b
BruceWhiteBai <1258444549@qq.com>
2018-11-22 19:33:19 +0800

    新增vue 和 thymeleaf模块

---
+++ vue-web/src/main/java/com/bruce/vueweb/VueWebApplication.java
@@ -0,0 +1,12 @@
+package com.bruce.vueweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueWebApplication.class, args);
+    }
+}
