1e43d8ecc27eda298b7e450a17176c1ae89c9ab2
BruceWhiteBai <1258444549@qq.com>
2018-12-06 18:18:11 +0800

    1.引入rocketmq

---
+++ mq/rocket/src/main/java/com/bruce/rocket/config/PropsConfig.java
@@ -0,0 +1,31 @@
+package com.bruce.rocket.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ConfigurationProperties(prefix = "suning.rocketmq")
+@Data
+public class PropsConfig {
+
+    private String namesrvAddr;
+    private String producerGroupName;
+    private String transactionProducerGroupName;
+    private String consumerGroupName;
+    private String producerInstanceName;
+    private String consumerInstanceName;
+    private String producerTranInstanceName;
+    private int consumerBatchMaxSize;
+    private boolean consumerBroadcasting;
+    private boolean enableHistoryConsumer;
+    private boolean enableOrderConsumer;
+    private List<String> subscribe = new ArrayList<String>();
+
+}
