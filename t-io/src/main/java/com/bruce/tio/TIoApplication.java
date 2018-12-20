1b79b409af96614064957054756395c49ba9d20d
BruceWhiteBai <1258444549@qq.com>
2018-12-20 16:30:17 +0800

    t-io集成

---
+++ t-io/src/main/java/com/bruce/tio/TIoApplication.java
@@ -0,0 +1,14 @@
+package com.bruce.tio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TIoApplication.class, args);
+    }
+
+}
+
