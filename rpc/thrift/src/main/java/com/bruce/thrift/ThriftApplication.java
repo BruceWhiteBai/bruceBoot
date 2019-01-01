4bbca4e7ec857bd1945eb8cea68f685a836e2a19
BruceWhiteBai <1258444549@qq.com>
2019-01-01 19:46:31 +0800

    添加rpc模块和子模块

---
+++ rpc/thrift/src/main/java/com/bruce/thrift/ThriftApplication.java
@@ -0,0 +1,14 @@
+package com.bruce.thrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThriftApplication.class, args);
+    }
+
+}
+
