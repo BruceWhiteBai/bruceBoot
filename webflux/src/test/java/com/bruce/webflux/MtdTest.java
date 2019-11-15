b9e4c604e6b8c82a41ec97cd378becee4db0cd15
BruceWhiteBai <1258444549@qq.com>
2019-11-15 17:59:16 +0800

    mtd test

--- webflux/src/test/java/com/bruce/webflux/MtdTest.java
+++ webflux/src/test/java/com/bruce/webflux/MtdTest.java
@@ -9,10 +9,26 @@ public class MtdTest {
         return;
     }
 
+    private void testException(){
+        try{
+            System.out.println(1/0);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
     public void test01(){
         System.out.println("0----------------");
         testOut();
+        System.out.println("2----------------");
+
+    }
+
+    @Test
+    public void test02(){
+        System.out.println("0----------------");
+        testException();
         System.out.println("2----------------");
 
     }
